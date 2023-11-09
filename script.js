cccbb68aaa905c4e3f66546b371ae7bea40b4def
“[dfitzpatrick13]” <[danfitz156@gmail.com]>
2023-11-09 16:00:00 -0500

    changes

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+//function for chocices 
+function startGame(playersChoice){
+    //player choice 
+    const playersChoice = ["rock","paper","scissors"]
+    //  pc choice 
+    computerChoice = playersChoice[Math.floor(Math.random() * 3 )]
+
+
+}
