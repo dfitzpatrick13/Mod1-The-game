af6d5a9f439c947b9d4507f0fbd392d36196999e
Daniel Fitzpatrick <danfitz156@gmail.com>
2023-11-10 20:10:09 -0500

    changes to the js

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 
 
-//function for chocices 
-function startGame(playersChoice){
-    //player choice 
-    const playersChoice = ["rock","paper","scissors"]
-    //  pc choice 
-    computerChoice = playersChoice[Math.floor(Math.random() * 3 )]
-
-
+//function for choices 
+function startGame() {
+    //player choices 
+    const choices = ["rock","paper","scissors"]
+    //  pc choicess
+    computerChoice = choices[Math.floor(Math.random() * 3 )]
+    
+    return {
+        playerChoice: choices[0]
+        ,computerChoice: computerChoice
+    }
 }
+
+
+ 
+const playerChoice = gameChoices.playerChoice;
+const computerChoice = gameChoices.computerChoice;
+
+
+
+const gameChoices = startGame()
+
+//result is determening who wins the game based on a string
+
+const result = " "
+
+// if statement that handles the result of a game 
+
+if (playerChoice === computerChoice) {
+    const result = 'It\'s a tie!';
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = 'You win!';
+  } else {
+    result = 'You lose!';
+  }
+  // inner html stuff that  may delete later
+//   document.getElementById('result').innerHTML = 'You chose ' + playerChoice +
+//         '. Computer chose ' + computerChoice + '. ' + result;
+
+    
+        //
+
+        const rockButton = document.getElementById("Rock")
+
+      
+        
+  
+  
+
+    
+    
